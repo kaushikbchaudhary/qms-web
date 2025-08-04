70925b93d1b4de1f32f5c45c06e83cd2e7cf4e2b
Kaushik chaudhary <kaushik.chaudhary@projectkmt.com>
2025-08-04 17:19:19 +0530

    proxy.js file added for start ssl

---
+++ proxy.js
@@ -0,0 +1,22 @@
+// proxy.js
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const https = require('https');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const httpProxy = require('http-proxy');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const fs = require('fs');
+
+const proxy = httpProxy.createProxyServer({});
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+};
+
+https
+    .createServer(options, (req, res) => {
+        proxy.web(req, res, { target: 'http://localhost:3000' });
+    })
+    .listen(3001, () => {
+        console.log('🔐 HTTPS Proxy running at https://0.0.0.0:3001');
+    });
